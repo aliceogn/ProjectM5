18204b72c0d020db330bed5ef00a124af81173b4
sofiamv98 <sofismv98@gmail.com>
2022-03-14 11:46:10 +0100

    Acabada classe Article i interface Visor, començada classe VisorTxt

--- Article.java
+++ Article.java
@@ -1,5 +1,17 @@
-// Aquest programa presenta la classe Article per al projecte App Llista de la compra 
+// Aquest programa presenta la classe Article.java per al projecte App Llista de la compra
+
 public class Article {
     private String nom;
     private Categoria categoria;
+
+    public String getNom() { return nom; }
+    public Categoria getCategoria() { return categoria; }
+    public void setNom(String nom) {
+        if (!nom.isBlank()) {
+            this.nom = nom;
+        }
+    }
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
---
+++ VisorTxt.java
@@ -0,0 +1,10 @@
+// Aquesta classe implementa la interfície Visor i formateja una Llista en format txt
+
+public class VisorTxt implements Visor {
+    public String mostra(Llista llista) {
+        return "";
+    } 
+    public String mostra(Llista llista, Categoria categoria) {
+        return "";
+    }
+}
