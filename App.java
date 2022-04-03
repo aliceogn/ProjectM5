324aaef26bac4c595e492b62684b45d3804289ed
sofiamv98 <sofismv98@gmail.com>
2022-04-03 22:35:07 +0200

    Avançada classe App

--- App.java
+++ App.java
@@ -4,39 +4,47 @@ public class App {
     private Visor Visor;
     private Llista llista;
     private Categoria[] categories;
+    private static App instancia;
 
+    private App() { }
+    // fem un singleton perquè només utilitzarem una App
+    private static App getApp() { 
+        if (instancia == null) {
+            instancia = new App();
+        }
+        return instancia;
+    }
     // Esta función muestra por pantalla estándar las categorías disponibles
     private void mostraCategories() {
         for (int i = 0; i < categories.length; i++) {
-            System.out.println(categories[i]);
+            System.out.println(categories[i]);   // fer toString
         }
     }
     private void afegeixCategoria(String nom) {
-        Article[] articles;
-        Categoria categoria = new Categoria(nom, articles);   // creem una nova categoria
+        Categoria categoria = new Categoria(nom);   // creem una nova categoria
         categories = Arrays.copyOf(categories, categories.length + 1 );   // copiem l'array de categories que teniem i la fem 1 posició més gran
         categories[categories.length - 1] = categoria;    // aquí guardem la nova categoria a la nova última posició de l'array
     }
-    private void esborraCategoria(String nom) {
+    private void esborraCategoria(Categoria categoria) {
+        // esborrar els articles de la categoria abans
+        
         Categoria[] newArray = new Categoria[categories.length - 1];
 
         for (int i = 0; i < categories.length; i++) {
-            if (categories[i].getNom() != nom) { 
+            if (categories[i].getNom() != categoria.getNom()) { 
                 int j = 0;
-                newArray[j++] = categories[i];
+                newArray[j] = categories[i];
+                j++;
             }
-            // mirar funcionalitat per a comprovar si el nom de la categoria coincideix amb una existent
         }
         this.categories = newArray;
     }
-    private void afegirArticle(String nom) {
-        Article[] articles;
-        Article article = new Article(nom, categories);   // creem una nova categoria
-        categories = Arrays.copyOf(categories, categories.length + 1 );   // copiem l'array de categories que teniem i la fem 1 posició més gran
-        categories[categories.length - 1] = categoria;
+    private void afegirArticle(String nom, Categoria categoria) {
+        categoria.createArticle(nom);
     }
-    private void esborrarArticle() {
-
+    private void esborrarArticle(Article article) {
+        // esborrar items abans
+        Article[] articles = article.getCategoria().getArticles();
     }
     private void afegirItem() {
 
@@ -53,6 +61,15 @@ public class App {
     private String mostraLlista(Categoria categoria) {
 
     }
+    // Aquest mètode busca, a partir del nom donat, la categoria a la qual pertany i la retorna
+    private Categoria trobaCategoria(String nom) {
+        for (Categoria categoria: categories) {
+            if (categoria.getNom().equals(nom)) {
+                return categoria;
+            }
+        }
+        return null;  // si no ha trobat la categoria amb el nom que se li ha passat retornem null
+    }
     private static void mostraMenuPrincipal () {
         System.out.println("MENU PRINCIPAL\n");
         System.out.println("Opcions: \n");
@@ -66,44 +83,56 @@ public class App {
         System.out.println("7. Seleccionar visor");
         System.out.println("8. Mostrar llista");
         System.out.println("9. Mostrar llista per categories");
+        System.out.println("10. Sortir del programa");
         System.out.println("\nIndica la opció escollida");
     }
     public static void main(String[] args) {
-        App.mostraMenuPrincipal();
-        int numEscollit = Integer.parseInt(Entrada.readLine());
-        switch(numEscollit) {
-            case 0: System.out.println("Anomena la nova categoria");
-                    String novaCategoria = Entrada.readLine();
-                    afegeixCategoria(novaCategoria); // mirar array de artículos??
-            case 1: mostraCategories();
+        App app = getApp();
+        boolean volSortir = false;
+        while(!volSortir) {
+            mostraMenuPrincipal();
+            int numEscollit = Integer.parseInt(Entrada.readLine());
+            switch(numEscollit) {
+                case 0:
+                    System.out.println("Anomena la nova categoria");
+                    app.afegeixCategoria(Entrada.readLine()); // mirar array de artículos??
+                    break;
+                case 1:
+                    app.mostraCategories();
                     System.out.println("Quina categoria vol esborrar?");
-                    String categoriaAEsborrar = Entrada.readLine();
+                    Categoria categoriaAEsborrar = app.trobaCategoria(Entrada.readLine());
                     System.out.println("Està segur?");
                     System.out.println("\n0. No");
                     System.out.println("\n1. Si");
                     int resposta = Integer.parseInt(Entrada.readLine());
                     if (resposta == 1) {
-                        esborraCategoria(categoriaAEsborrar);
+                        app.esborraCategoria(categoriaAEsborrar);
                     }
                     else {
                         mostraMenuPrincipal();
                     }
-            case 2: System.out.println("Anomena el nou article");
-                    String nouArticle = Entrada.readLine();
+                    break;
+                case 2: 
+                    System.out.println("Anomena el nou article");
+                    String nomArticle = Entrada.readLine();
                     System.out.println("A quina categoria pertany?");
-                    Categoria categoriaEscollida = Entrada.readLine();
-                    afegirArticle(nouArticle, );
-            case 3: System.out.println("Quin article vol esborrar?");
+                    Categoria categoriaEscollida = app.trobaCategoria(Entrada.readLine());
+                    app.afegirArticle(nomArticle, categoriaEscollida);
+                    break;
+                case 3: 
+                    System.out.println("Quin article vol esborrar?");
                     String articleAEsborrar = Entrada.readLine();
                     System.out.println("Està segur?");
                     System.out.println("\n0. No");
                     System.out.println("\n1. Si");
                     int resposta = Integer.parseInt(Entrada.readLine());
                     if (resposta == 1) {
-                        esborraCategoria(categoriaAEsborrar);
+                        app.esborraCategoria(categoriaAEsborrar);
                     } 
                     else {
                         mostraMenuPrincipal();
+                    }
+            }
         }
     }
 }
--- Article.java
+++ Article.java
@@ -4,6 +4,11 @@ public class Article {
     private String nom;
     private Categoria categoria;
 
+    public Article(String nom, Categoria categoria) {
+        setNom(nom);
+        setCategoria(categoria);
+    }
+
     public String getNom() { return nom; }
     public Categoria getCategoria() { return categoria; }
     public void setNom(String nom) {
